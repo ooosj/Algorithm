f22f17ac59d01850fb9f85eb5ab2cc9cefc27021
오승주 <tmdwn8775@naver.com>
2026-03-25 16:55:32 +0900

    0325

--- boj1149/Main.java
+++ boj1149/Main.java
@@ -1,57 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
         int n = Integer.parseInt(br.readLine());
 
-        int[][] rgb = new int[n+1][4];
-        int[] check = new int[n+2];
-        PriorityQueue <int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0]-o2[0];
-            }
-        });
+        int[][] cost = new int[n][3];
+        int[][] dp = new int[n][3];
 
-        for(int i = 1 ; i < n+1 ; i++){
-            st = new StringTokenizer(br.readLine());
-
-            for(int j = 1 ; j < 4 ; j++){
-                int t = Integer.parseInt(st.nextToken());
-                rgb[i][j] = t;
-                pq.add(new int[]{t, i, j});
-            }
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cost[i][0] = Integer.parseInt(st.nextToken()); // R
+            cost[i][1] = Integer.parseInt(st.nextToken()); // G
+            cost[i][2] = Integer.parseInt(st.nextToken()); // B
         }
 
-        int cnt = 0;
-        int result = 0;
+        // 첫 번째 집은 초기값 그대로 설정
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
 
-        for(int i = 0 ; i < 3 * n ; i++){
-            int[] temp = pq.poll();
-
-            int r = temp[1];
-            int c = temp[2];
-            int v = temp[0];
-
-            if(check[r-1] != c && check[r+1] != c && check[r] == 0){
-                check[r] = c;
-                cnt++;
-                result += v;
-            }
-
-            if(cnt == n) break;
-
+        // 두 번째 집부터 점화식 적용
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
         }
 
-        System.out.println(result);
-
+        // 마지막 집의 세 가지 경우 중 최솟값 출력
+        System.out.println(Math.min(Math.min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]));
     }
 }
